461544235a2f457dc00fd627f7a4863f51f58ce8
gxx <562606139@qq.com>
2016-05-03 23:28:12 +0800

    5.3

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const open = require('open');
 new WebpackDevServer(webpack(config), config.devServer)
 .listen(config.port, 'localhost', (err) => {
   if (err) {
-    console.log(err);
+    console.log(err);no
   }
   console.log('Listening at localhost:' + config.port);
   console.log('Opening your system browser...');
